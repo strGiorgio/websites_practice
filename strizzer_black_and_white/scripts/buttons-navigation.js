81abc2019b029cf7d42959987c09df20df9e5203
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-01 15:52:12 -0300

    When nav button clicked, circle indication change

--- strizzer_black_and_white/scripts/buttons-navigation.js
+++ strizzer_black_and_white/scripts/buttons-navigation.js
@@ -1,9 +1,13 @@
 const btnPrevious = document.querySelector('.previous-arrow');
 const btnNext = document.querySelector('.next-arrow');
 
-const img0 = document.querySelector('.image-banner0');
-const img1 = document.querySelector('.image-banner1');
-const img2 = document.querySelector('.image-banner2');
+const img0 = document.querySelector('.image-banner.image-0');
+const img1 = document.querySelector('.image-banner.image-1');
+const img2 = document.querySelector('.image-banner.image-2');
+
+const circle0 = document.querySelector('.circle.page-0')
+const circle1 = document.querySelector('.circle.page-1')
+const circle2 = document.querySelector('.circle.page-2')
 
 var locate = 1;
 
@@ -31,6 +35,11 @@ function changeImg() {
             img0.classList.add('active')
             img1.classList.remove('active')
             img2.classList.remove('active')
+
+            circle0.classList.add('fill')
+            circle1.classList.remove('fill')
+            circle2.classList.remove('fill')
+
             console.log(locate)
             break
 
@@ -38,6 +47,11 @@ function changeImg() {
             img0.classList.remove('active')
             img1.classList.add('active')
             img2.classList.remove('active')
+
+            circle0.classList.remove('fill')
+            circle1.classList.add('fill')
+            circle2.classList.remove('fill')
+
             console.log(locate)
             break
             
@@ -45,6 +59,11 @@ function changeImg() {
             img0.classList.remove('active')
             img1.classList.remove('active')
             img2.classList.add('active')
+
+            circle0.classList.remove('fill')
+            circle1.classList.remove('fill')
+            circle2.classList.add('fill')
+
             console.log(locate)
             break
     }
