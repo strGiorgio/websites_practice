33f47c98f1f548685e32500da2e912539c8775aa
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-05 16:39:57 -0300

    Delete useless code and import new components

--- midnight/src/App.vue
+++ midnight/src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mainSection />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import mainSection from './components/main-section.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    mainSection
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  :root {
+    --blue-primary: #061A40;
+    --blue-dark: #031029;
+    --blue-secondary: #4D7C8A;
+    --blue-light: #D2D6EF;
+    --yellow-primary: #EFA00B; 
+    --orange-primary: #D65108;
+
+    --font-primary: 'Montserrat', sans-serif;; 
+  }
+
+  html {
+    font-size: 10px;
+  }
+
+  * {
+     padding: 0;
+     margin: 0;
+   }
+
+  body {
+     display: flex;
+     justify-content: center;
+     background-image: url('/imgs/sky.jpg');
+     background-size: cover;
+     background-repeat: no-repeat;
+   }
+
 </style>
