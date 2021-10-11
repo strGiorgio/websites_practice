0537a40b1d6a08a7e4c86e50382eb7a2c7adc06c
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-11 15:56:07 -0300

    Create a main footer to website

---
+++ midnight/src/components/main-footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main-footer">
+        <p>strGiorgio &copy; 2021</p>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'mainFooter'
+}
+</script>
+
+<style scoped>
+    .main-footer {
+        position: absolute;
+        bottom: 10px;
+        margin: o auto;
+        left: calc(50% - 100px);
+    }
+
+    .main-footer p {
+        color: var(--blue-light);
+        font: 1.5rem var(--font-primary);
+    }
+</style>
