c4e746c837a1a8b9f440b63c4f635c5a4f72e381
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-21 16:56:03 -0300

    Create a requisition to post User in database

--- admin_dashboard/src/store/index.js
+++ admin_dashboard/src/store/index.js
@@ -52,9 +52,14 @@ export default new Vuex.Store({
       }
 
       if (context.state.userExist) {
-        console.log('Cant Post')
+        console.log('Can`t Post')
       } else {
-        console.log('Can Post')
+        const postUser = await fetch('http://localhost:3000/users', {
+          method: 'POST',
+          headers: {"Content-Type" : "application/json"},
+          body: JSON.stringify(payload)
+        });
+        console.log(postUser)
       }
     }
   },
