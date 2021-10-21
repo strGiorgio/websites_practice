72f8a373b978a6dbffad3bda9d2bab45cddbf21f
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-21 15:56:54 -0300

    Finalize function action, creating a else statement to post user

--- admin_dashboard/src/store/index.js
+++ admin_dashboard/src/store/index.js
@@ -34,12 +34,22 @@ export default new Vuex.Store({
       const users = await userDB.json();
 
       for(var i in users) {
+        //Verify if user name or email already exists
         if(payload.name == users[i].name && payload.email == users[i].email) {
           console.log('Name and Email Already Exists!')
         } else if(payload.name == users[i].name) {
             console.log('Name Already Exists!')
           } else if(payload.email == users[i].email) {
             console.log('Email Already Exists!')
+          } else {
+            //Post User
+            const postUser = await fetch('http://localhost:3000/users', {
+              method: 'POST',
+              headers: {"Content-Type" : "application/json"},
+              body: JSON.stringify(payload)
+            });
+            const resPostUser = await postUser.json()
+            console.log(resPostUser)
           }
       }
     }
