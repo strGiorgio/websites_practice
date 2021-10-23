6f774fad1497d6bf8d4f198c6873460db6089443
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-23 13:36:16 -0300

    Create a folder for components in dashboard

---
+++ admin_dashboard/src/components/dashboard-comps/main-dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main-dashboard">
+        <p>{{ this.$store.state.isAdmin }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'mainDashboard'    
+}
+</script>
+
+<style scoped>
+
+</style>
