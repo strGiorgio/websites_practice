460fbede9aada0a7065df034f9db0116dc7b621c
Giorgio Alefe <giorgioa2002@gmail.com>
2021-11-11 15:12:52 -0300

    Create class list toggle in displayFAQS

--- techlogic/scripts/displayFAQS.js
+++ techlogic/scripts/displayFAQS.js
@@ -7,19 +7,20 @@ for (var i = 0; i < icon.length; i ++) {
 
 function activate(e) {
     //BOX
-    for (var i = 0; i < box.length; i ++) {
-        box[i].classList.remove('active')
-    }
+    //for (var i = 0; i < box.length; i ++) {
+    //    box[i].classList.remove('active')
+    //}
 
     for (var i in box) {
         if (box[i].id == this.id) {
             const boxText = document.querySelector(`article>#${box[i].id}`)
-            boxText.classList.add('active')
+            boxText.classList.toggle('active')
         }
     }
     //ICON
-    for (var i = 0; i < icon.length; i ++) {
-        icon[i].classList.remove('active')
-    }
-    this.classList.add('active')
+    //for (var i = 0; i < icon.length; i ++) {
+    //    icon[i].classList.remove('active')
+    //}
+    this.classList.toggle('active')
+    console.log(e)
 }
